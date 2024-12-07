59741120d547b9244063d663174522fa14a894c1
gsistelos <gsistelos@proton.me>
2024-12-07 16:22:15 -0300

    prometheus metrics and middleware

--- api/server.go
+++ api/server.go
@@ -5,8 +5,30 @@ import (
 	"log/slog"
 	"net/http"
 
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+var (
+	httpRequestsTotal = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Total number of HTTP requests",
+		},
+		[]string{"method", "route", "status"},
+	)
+
+	httpRequestDuration = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "http_request_duration_seconds",
+			Help:    "Histogram of HTTP request durations",
+			Buckets: prometheus.DefBuckets,
+		},
+		[]string{"method", "route"},
+	)
+)
+
+type handlerFunc func(http.ResponseWriter, *http.Request) (int, any)
 
 type Server struct{}
 
@@ -15,14 +37,19 @@ func NewServer() *Server {
 }
 
 func (s *Server) Run(listenAddr string) error {
+	prometheus.MustRegister(httpRequestsTotal)
+	prometheus.MustRegister(httpRequestDuration)
+
 	http.Handle("/metrics", promhttp.Handler())
+
+	http.Handle("/health", promMiddleware(healthHandler))
 
 	slog.Info("Server started", "ListenAddr", listenAddr)
 
 	return http.ListenAndServe(listenAddr, nil)
 }
 
-func writeJson(w http.ResponseWriter, status int, v any) {
+func writeJson(w http.ResponseWriter, status int, v any) (int, any) {
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(status)
 
@@ -30,4 +57,6 @@ func writeJson(w http.ResponseWriter, status int, v any) {
 		slog.Error("writeJson: failed to encode")
 		panic(err)
 	}
+
+	return status, v
 }
